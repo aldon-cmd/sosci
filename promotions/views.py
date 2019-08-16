4035ad49c0d4959d52a0086c0969470b52a45a99
wiel <wielidon@gmail.com>
2019-08-15 22:11:18 -0500

    fixed some broken urls

--- promotions/views.py
+++ promotions/views.py
@@ -2,6 +2,17 @@
 from __future__ import unicode_literals
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from customer.forms import CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.shortcuts import redirect
 
-class LandingView(TemplateView):
+class LandingView(auth_views.LoginView):
     template_name = "promotions/landing.html"
+    form_class = CustomAuthenticationForm
+
+    def form_valid(self, form):
+
+        """Security check complete. Log the user in."""
+        auth_login(self.request, form.get_user())
+        return redirect('course:course-list')    
