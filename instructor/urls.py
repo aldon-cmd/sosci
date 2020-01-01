a92548bd86e4019764a6baddb2e1e6b22044e091
wiel <wielidon@gmail.com>
2020-01-01 10:11:12 -0500

    fix on demand upload

--- instructor/urls.py
+++ instructor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
 from catalogue import views as catalogue_views
 from livestream import views as livestream_views
+from video import views as video_views
 
 app_name = 'instructor'
 
@@ -10,7 +11,8 @@ urlpatterns = [
         re_path(r'^publish/course/(?P<course_id>\d+)/$', catalogue_views.PublishCourseView.as_view(), name='publish-course'),
 	    re_path(r'^module/create/(?P<course_id>\d+)/$', catalogue_views.ModuleCreateView.as_view(), name='module-create-form'),
         re_path(r'^live/module/create/(?P<course_id>\d+)/$', catalogue_views.LiveModuleCreateView.as_view(), name='live-module-create-form'),
-        re_path(r'^student/list/$', catalogue_views.StudentListView.as_view(), name='student-list'),	    
+        re_path(r'^student/list/$', catalogue_views.StudentListView.as_view(), name='student-list'),
+        re_path(r'^upload/attempt/(?P<course_id>\d+)/$', video_views.VimeoVideoUploadAttemptView.as_view(), name='video-upload-attempt'),	    
 	    re_path(r'^start-session/(?P<course_id>\d+)/$', livestream_views.StartSessionView.as_view(), name='start-session'),
 	    re_path(r'^end-session/(?P<course_id>\d+)/$', livestream_views.EndSessionView.as_view(), name='end-session'),
 	    re_path(r'^room-status/$', livestream_views.TwilioRoomStatusView.as_view(), name='twilio-room-status'),
