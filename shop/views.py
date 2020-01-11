46efca77d4295c79ffcaf8e965a225ac96805d66
wiel <wielidon@gmail.com>
2020-01-11 11:49:58 -0500

    added popular and recent course query

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,7 @@ from django.views.generic.list import ListView
 from catalogue.utils import Course
 from catalogue import models as catalogue_models
 from django.views.generic import TemplateView
-
+from django.db.models import Count
 
 class Welcome1View(TemplateView):
     template_name = "shop/welcome1.html"
@@ -12,10 +12,18 @@ class Welcome1View(TemplateView):
 class Welcome2View(TemplateView):
     template_name = "shop/welcome2.html"
 
-class LandingView(ListView):
-    template_name = "shop/landing.html"
+class PopularLandingView(ListView):
+    template_name = "shop/popular_landing.html"
     paginate_by = 10
     model = catalogue_models.Product
 
     def get_queryset(self):
-        return Course().get_courses().filter(is_published=True)
+        return Course().get_courses().filter(is_published=True).annotate(enrolled_user_count=Count('enrollments__user') ).order_by('-enrolled_user_count')
+
+class RecentLandingView(ListView):
+    template_name = "shop/recent_landing.html"
+    paginate_by = 10
+    model = catalogue_models.Product
+
+    def get_queryset(self):
+        return Course().get_courses().filter(is_published=True).order_by('-date_created')        
